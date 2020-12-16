6518e2fe878ccbfc66010998b5fab50cd07bc2d2
Kanan Hooper <KananHooper@gmail.com>
2020-12-16 17:54:23 -0500

    updates to use callback

--- app.js
+++ app.js
@@ -10,39 +10,53 @@ const S3Bucket = process.env.bucket;
 // AWS Setup
 AWS.config.update(
     {
-      region: 'us-east-2',
-      accessKeyId: process.env.accessKeyId,
-      secretAccessKey: process.env.secretAccessKey
+        region: 'us-east-2',
+        accessKeyId: process.env.accessKeyId,
+        secretAccessKey: process.env.secretAccessKey
     }
 ); // us-east-2 is Ohio
 
 
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
+const FileName = 'deploy.tar.gz';
 
 const params = {
-  Bucket: 'gard-rarediseases-json',
-  Key: 'build/deploy.tar.gz'
+    Bucket: S3Bucket,
+    Key: `build/${FileName}`
 };
 
-console.log('Download static files');
-const fileStream = s3.getObject(params).createReadStream();
-const file = require('fs').createWriteStream('deploy.zip');
-fileStream.pipe(file);
+console.log(`Download ${FileName}`);
+const fileStream = fs.createWriteStream(FileName);
+const s3Stream = s3.getObject(params).createReadStream();
 
-
-console.log('Unzip deploy.zip');
-exec("tar -xf deploy.tar.gz -C /dist", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`Unzip deploy.zip - error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`Unzip deploy.zip - stderr: ${stderr}`);
-    return;
-  }
-  if(stdout) {
-    console.log(`Unzip deploy.zip: ${stdout}`);
-  }
+// Listen for errors returned by the service
+s3Stream.on('error', function (err) {
+    // NoSuchKey: The specified key does not exist
+    console.error(err);
 });
+
+s3Stream.pipe(fileStream).on('error', function (err) {
+    // capture any errors that occur when writing data to the file
+    console.error('File Stream:', err);
+}).on('close', function () {
+
+    console.log(`Unzip ${FileName}`);
+    exec("tar -xf deploy.tar.gz", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Unzip ${FileName} - error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`Unzip ${FileName} - stderr: ${stderr}`);
+            return;
+        }
+        if (stdout) {
+            console.log(`Unzip ${FileName}: ${stdout}`);
+        }
+    });
+
+});
+
+
 
